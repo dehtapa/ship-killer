98ba53457eebcbba6048a288f19edb13b7c575aa
Tamas Pataky <tamas.pataky@outlook.com>
2018-09-27 09:57:40 +0200

    tests

--- Main.py
+++ Main.py
@@ -2,35 +2,35 @@ from Player import *
 from Ship import *
 from Table import *
 from Window import *
-from Game import *
+import tkinter as tk
 
 
-class Main:
+class Game:
+    turn = 1
     window = None
+    table1 = None
+    table2 = None
     player1 = None
     player2 = None
 
-    def __init__(self, numberOfShips, rows):
+    def __init__(self, root, numberOfShips, rows):
+        self.root = root
         self.numberOfShips = numberOfShips
         self.rows = rows
-        self.game = Game()
         # Start process
         self.init_game()
-        self.end_game()
         
     def init_game(self):
-        print("AKJHSDJKH")
+        pass
         # Draw main window
-        self.window = Window(self, 1200, 900)
-        
+        # self.window = Window(self)        
         # Draw tables
-        self.table1 = Table(1, self, 0, 0)
-        self.table2 = Table(2, self, 0, 40)
+        #self.table1 = Table(1, self, 0, 0)
+        #self.table2 = Table(2, self, 0, 40)
         # Create players
         #self.player1 = Player('Player1')
         #self.player2 = Player('Player2')
         #self.skip_status()
-
 
     def auto_build_ships(self):
         pass
@@ -41,7 +41,13 @@ class Main:
     def clear_ships(self):
         pass
 
-    def end_game(self):
-        self.window.root.mainloop()
 
-game = Main(4, 5)
+def main(numberOfShips, rows, width, height): #run mianloop 
+    root = tk.Tk()
+    root.geometry("%sx%s" % (width, height))
+    app = Game(root, numberOfShips, rows)
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main(4, 10, 1200, 900)
--- Window.py
+++ Window.py
@@ -2,44 +2,33 @@ from tkinter import *
 
 
 class Window:
-    def __init__(self, master, width, height):
-        self.width = width
-        self.height = height
-        self.rows = master.rows
-        self.master = master
-        self.root = None
-        # init window
-        self.draw_main_window()
+    buttonCreateShips = None
+    buttonClearShips = None
 
-        self.message1 = StringVar(value="")
-        self.message2 = StringVar(value="")
-        self.message3 = StringVar(value="")
-        self.message4 = StringVar(value="")
-        self.message5 = StringVar(value="")
-        self.message6 = StringVar(value="")
-        self.message7 = StringVar(value="")
-        self.buttonCreateShips = None
-        self.buttonClearShips = None
+    def __init__(self, master):
+        self.master = master
+        message1 = StringVar(value="")
+        message2 = StringVar(value="")
+        message3 = StringVar(value="")
+        message4 = StringVar(value="")
+        message5 = StringVar(value="")
+        message6 = StringVar(value="")
+        message7 = StringVar(value="")
         # Start process
         self.init_messages()
         self.init_buttons()
 
-    def draw_main_window(self):
-        print("KJHKJ")
-        self.root = Tk()
-        self.root.geometry("%sx%s" % (self.width, self.height))
-
     def init_messages(self):
-        Label(self.root, textvariable=self.message1, fg="blue", font=("Helvetica", 16)).grid(row=0, column=20)
-        Label(self.root, textvariable=self.message2).grid(row=2, column=20)
-        Label(self.root, textvariable=self.message3, fg="blue", font=("Helvetica", 12)).grid(row=3, column=20)
-        Label(self.root, textvariable=self.message4).grid(row=4, column=20)
-        Label(self.root, textvariable=self.message5, fg="blue", font=("Helvetica", 12)).grid(row=5, column=20)
-        Label(self.root, textvariable=self.message6).grid(row=6, column=20)
-        Label(self.root, textvariable=self.message7, fg="red", font=("Helvetica", 16)).grid(row=7, column=20)
+        Label(self.master.root, textvariable=self.message1, fg="blue", font=("Helvetica", 16)).grid(row=0, column=20)
+        Label(self.master.root, textvariable=self.message2).grid(row=2, column=20)
+        Label(self.master.root, textvariable=self.message3, fg="blue", font=("Helvetica", 12)).grid(row=3, column=20)
+        Label(self.master.root, textvariable=self.message4).grid(row=4, column=20)
+        Label(self.master.root, textvariable=self.message5, fg="blue", font=("Helvetica", 12)).grid(row=5, column=20)
+        Label(self.master.root, textvariable=self.message6).grid(row=6, column=20)
+        Label(self.master.root, textvariable=self.message7, fg="red", font=("Helvetica", 16)).grid(row=7, column=20)
 
     def init_buttons(self):
-        self.buttonCreateShips = Button(self.root, text="Create ships automatically", command=self.master.auto_build_ships)
+        self.buttonCreateShips = Button(self.master.root, text="Create ships automatically", command=self.master.auto_build_ships)
         self.buttonCreateShips.grid(row=9, column=20)
-        self.buttonClearShips = Button(self.root, text="Clear ships", command=self.master.clear_ships)
+        self.buttonClearShips = Button(self.master.root, text="Clear ships", command=self.master.clear_ships)
         self.buttonClearShips.grid(row=11, column=20)
